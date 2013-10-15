5494944914ad33762238ae2f059c25e5fabc3534
gskielian <gregory.kielian@hackerdojo.com>
2013-10-15 00:10:37 -0700

    Create ggit.sh

---
+++ ggit.sh
@@ -0,0 +1,5 @@
+###simple alias for wrapping generic git commit (to include a message see gitfunction.sh)
+
+
+###add following line to your ~/.bash_profile or ~/.bash_rc
+alias ggit="git add * && git commit -m \"added some stuff\" && git push origin master"
